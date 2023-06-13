13de99370587ed28fa66f7942b84e8a6179490a0
kr4b <40267916+kr4b@users.noreply.github.com>
2023-06-13 08:09:18 +0200

    Add cheaty clique crossover

--- Variation.py
+++ Variation.py
@@ -56,7 +56,6 @@ def clique_crossover(individual_a: Individual, individual_b: Individual, p=0.5):
 	assert len(individual_a.genotype) == len(individual_b.genotype), "solutions should be equal in size"
 	assert len(individual_a.genotype) % 5 == 0, "instance should be a 5-clique chain"
 
-	# TODO: Do something with chain vertices
 	cliques = len(individual_a.genotype) // 5
 
 	l = len(individual_a.genotype)
@@ -66,5 +65,11 @@ def clique_crossover(individual_a: Individual, individual_b: Individual, p=0.5):
 	m = np.repeat(np.random.choice((0,1), p=(p, 1-p), size=cliques), 5)
 	offspring_a.genotype = np.where(m, individual_a.genotype, individual_b.genotype)
 	offspring_b.genotype = np.where(1 - m, individual_a.genotype, individual_b.genotype)
+
+	for genotype in [offspring_a.genotype, offspring_b.genotype]:
+		for i in range(1, cliques - 1):
+			# If chain vertices are in the same partition, flip the genotype of the second
+			if genotype[i*5-1] == genotype[i*5]:
+				genotype[i*5:(i+1)*5] = 1 - genotype[i*5:(i+1)*5]
 
 	return [offspring_a, offspring_b]
